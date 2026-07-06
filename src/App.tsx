ffaaa971e6daee572a86a32b48ada9edf35c1a1c
Ronald Lee King <aasotumediagroup@gmail.com>
2026-07-07 07:53:49 +0800

    debug: test layout components only

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,11 @@
 import { Routes, Route, useLocation } from 'react-router'
 import { useEffect, useRef, useCallback, useState } from 'react'
 import Lenis from 'lenis'
-import { trackConversion } from './lib/conversions'
 import Navigation from './components/Navigation'
 import MenuOverlay from './components/MenuOverlay'
 import Footer from './components/Footer'
 import CustomCursor from './components/CustomCursor'
 import AudioExperience from './components/AudioExperience'
-import HomePage from './pages/Home'
-import BlogPage from './pages/BlogPage'
-import BlogPostPage from './pages/BlogPostPage'
-import ServiceDetailPage from './pages/ServiceDetailPage'
-import WorkWithMe from './pages/WorkWithMe'
-import CivicsPage from './pages/CivicsPage'
-import WritingServicesPage from './pages/WritingServicesPage'
-import PrivacyPolicyPage from './pages/PrivacyPolicyPage'
-import IndigenousHeritagePage from './pages/IndigenousHeritagePage'
-import AdminDashboard from './pages/AdminDashboard'
-import AdminLogin from './pages/AdminLogin'
-import ProtectedAdminRoute from './components/ProtectedAdminRoute'
-import NotFound from './pages/NotFound'
-import AncestorRealmPage from './pages/AncestorRealmPage'
-import AncestorChatPage from './pages/AncestorChatPage'
-import SacredGalleryPage from './pages/SacredGalleryPage'
-import StoryKeeperPage from './pages/StoryKeeperPage'
-import AncestorPassPage from './pages/AncestorPassPage'
-import AncestorRootRegistryPage from './pages/AncestorRootRegistryPage'
-import RootRegistryStep1 from './pages/RootRegistryStep1'
-import NationResearchPage from './pages/NationResearchPage'
-import PreOrderPage from './pages/PreOrderPage'
-import PreOrderSuccessPage from './pages/PreOrderSuccessPage'
-import AboutAuthorPage from './pages/AboutAuthorPage'
 
 function AppLayout({ children }: { children: React.ReactNode }) {
   const [menuOpen, setMenuOpen] = useState(false)
@@ -39,19 +14,10 @@ function AppLayout({ children }: { children: React.ReactNode }) {
   const hideNav = location.pathname === '/ancestor-root-registry'
 
   useEffect(() => {
-    const lenis = new Lenis({
-      duration: 1.2,
-      easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      lerp: 0.1,
-    })
+    const lenis = new Lenis({ duration: 1.2, easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), lerp: 0.1 })
     lenisRef.current = lenis
-
-    function raf(time: number) {
-      lenis.raf(time)
-      requestAnimationFrame(raf)
-    }
+    function raf(time: number) { lenis.raf(time); requestAnimationFrame(raf) }
     requestAnimationFrame(raf)
-
     return () => { lenis.destroy() }
   }, [])
 
@@ -77,48 +43,15 @@ function AppLayout({ children }: { children: React.ReactNode }) {
 
 function AppRoutes() {
   const location = useLocation()
-
-  useEffect(() => {
-    trackConversion('PageView')
-  }, [location.pathname])
-
   return (
     <AppLayout>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/blog" element={<BlogPage />} />
-        <Route path="/blog/:slug" element={<BlogPostPage />} />
-        <Route path="/services/:slug" element={<ServiceDetailPage />} />
-        <Route path="/work-with-me" element={<WorkWithMe />} />
-        <Route path="/civics" element={<CivicsPage />} />
-        <Route path="/writing-services" element={<WritingServicesPage />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
-        <Route path="/indigenous-heritage" element={<IndigenousHeritagePage />} />
-        <Route path="/admin" element={<ProtectedAdminRoute><AdminDashboard /></ProtectedAdminRoute>} />
-        <Route path="/admin/blog" element={<ProtectedAdminRoute><AdminDashboard /></ProtectedAdminRoute>} />
-        <Route path="/admin/services" element={<ProtectedAdminRoute><AdminDashboard /></ProtectedAdminRoute>} />
-        <Route path="/admin/legal" element={<ProtectedAdminRoute><AdminDashboard /></ProtectedAdminRoute>} />
-        <Route path="/admin/setup" element={<ProtectedAdminRoute><AdminDashboard /></ProtectedAdminRoute>} />
-        <Route path="/admin/login" element={<AdminLogin />} />
-        <Route path="/ancestor-realm" element={<AncestorRealmPage />} />
-        <Route path="/ancestor-chat" element={<AncestorChatPage />} />
-        <Route path="/sacred-gallery" element={<SacredGalleryPage />} />
-        <Route path="/story-keeper" element={<StoryKeeperPage />} />
-        <Route path="/ancestor-pass" element={<AncestorPassPage />} />
-        <Route path="/ancestor-root-registry" element={<AncestorRootRegistryPage />} />
-        <Route path="/root-registry/step-1" element={<RootRegistryStep1 />} />
-        <Route path="/research/:nationId" element={<NationResearchPage />} />
-        <Route path="/pre-order" element={<PreOrderPage />} />
-        <Route path="/pre-order/success" element={<PreOrderSuccessPage />} />
-        <Route path="/about-author" element={<AboutAuthorPage />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="*" element={<div style={{ padding: '100px', color: 'red', fontSize: '40px' }}>LAYOUT WORKS</div>} />
       </Routes>
     </AppLayout>
   )
 }
 
-function App() {
+export default function App() {
   return <AppRoutes />
 }
-
-export default App
