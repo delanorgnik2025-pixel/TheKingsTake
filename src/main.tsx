550e115ab6bc69d70b86f2b74c163182ee80e161
Ronald Lee King <aasotumediagroup@gmail.com>
2026-07-07 07:45:47 +0800

    debug: test react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router'
 
 document.body.style.backgroundColor = '#0f0'
 const rootEl = document.getElementById('root')
 if (rootEl) {
-  createRoot(rootEl).render(<div style={{ color: 'red', fontSize: '40px', padding: '50px' }}>REACT WORKS</div>)
+  createRoot(rootEl).render(
+    <BrowserRouter>
+      <div style={{ color: 'red', fontSize: '40px', padding: '50px' }}>ROUTER WORKS</div>
+    </BrowserRouter>
+  )
 }
